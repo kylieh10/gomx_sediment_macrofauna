9e5effbd55c0997b47ee6baac5bb15ead0770607
Kylie <kylieh10@live.com>
2024-08-02 12:43:33 -0400

    Updated infauna_sample table with newly mapped terms and mutations

--- scripts/kylie_notebook.R
+++ scripts/kylie_notebook.R
@@ -5,6 +5,7 @@
 
 library(dplyr)
 library(sbtools)
+library(stringr)
 
 # Read data from ScienceBase ----------------------------------------------
 # link to data: https://www.sciencebase.gov/catalog/item/5a709594e4b0a9a2e9d88e4e
@@ -24,6 +25,7 @@ SedChem <- readr::read_csv(file = sb_filenames$url[3])
 #events representing unique cores (coreID)
 
 Infauna_StationCore <- Infauna %>%
+  
   rename(
     locationRemarks = Location,
     materialEntityID = CoreID,
@@ -31,13 +33,13 @@ Infauna_StationCore <- Infauna %>%
     decimalLatitude = Latitude,
     decimalLongitude = Longitude
   ) %>%
+  
   mutate(
     geodeticDatum = "WGS84",
     eventDate = DateCollected %>% 
       as.Date("%m/%d/%Y"),
     eventID = paste(Site, eventDate %>% as.character(), locationID, materialEntityID,
                     sep = "_") %>% stringr::str_remove_all(pattern = "-"),
-    # footprintWKT = NA, #get polygon from BOEM; not sure if we should include since event is lat/long specific?
     minimumDepthInMeters = Depth,
     maximumDepthInMeters = Depth,
     locality = paste("BOEM Lease Block", Site),
@@ -46,6 +48,7 @@ Infauna_StationCore <- Infauna %>%
                                   Site), sep = " | "),
     samplingProtocol = paste(Gear, CoreDiameter, sep = "_")
   ) %>%
+  
   select(
     eventID,
     eventDate,
@@ -56,38 +59,68 @@ Infauna_StationCore <- Infauna %>%
     higherGeography,
     locality,
     geodeticDatum,
-    # footprintWKT,
     minimumDepthInMeters,
     maximumDepthInMeters,
     samplingProtocol,
     locationRemarks
-  )
+  ) %>%
 
+  distinct()
 
 # Sample Level Event Table -------------------------------------------------
 
-## need to split fraction into upper and lower limit (maximumDistanceAboveSurface & minimum...)
-## not sure if there is a way to use dplyr for this, but the internet likes tidyr and stringr)
 
-Infauna_Sample <- Infauna_StationCore %>% 
+Infauna_Sample <- Infauna %>% 
+  
   rename(
-    parentEventID = eventID,
-    eventID = materialEntityID
-  ) %>% 
- 
+    eventID = CoreID,
+    locationRemarks = Location,
+    materialEntityID = SampleID,
+    locationID = Station,
+    decimalLatitude = Latitude,
+    decimalLongitude = Longitude
+  ) %>%
+  
+  mutate(
+    geodeticDatum = "WGS84",
+    eventDate = DateCollected %>% 
+      as.Date("%m/%d/%Y"),
+    parentEventID = paste(Site, eventDate %>% as.character(), locationID, materialEntityID,
+                    sep = "_") %>% stringr::str_remove_all(pattern = "-"),
+    minimumDepthInMeters = Depth,
+    maximumDepthInMeters = Depth,
+    locality = paste("BOEM Lease Block", Site),
+    higherGeography = paste("Gulf of Mexico",
+                            paste("BOEM Lease Block", 
+                                  Site), sep = " | "),
+    samplingProtocol = paste(Gear, CoreDiameter, sep = "_"),
+    Fraction=str_extract(Fraction, pattern= ".*\\d"),
+    #splitting fraction into new columns for upper limit and lower limit
+    maximumDistancesAboveSurfaceInMeters = str_split_i(
+        Fraction, pattern = "-", i = 2) %>% 
+      as.numeric()/-100,
+    minimumDistanceAboveSurfaceInMeters = str_split_i(Fraction, pattern = "-", i = 1) %>% 
+      as.numeric()/-100
+      ) %>% 
+  
   select(
-    decimalLatitude,
-    decimalLongitude,
-    locationRemarks,
-    samplingProtocol,
-    eventDate,
     eventID,
     parentEventID,
-    maximumDepthInMeters,
+    eventDate,
+    locationID,
+    decimalLatitude,
+    decimalLongitude,
+    higherGeography,
+    locality,
+    geodeticDatum,
     minimumDepthInMeters,
-    geodeticDatum
-    #and fraction once it's split
-  )
+    maximumDepthInMeters,
+    samplingProtocol,
+    locationRemarks,
+    minimumDistanceAboveSurfaceInMeters,
+    maximumDistancesAboveSurfaceInMeters
+  ) %>% 
+  distinct()
 
 
 
